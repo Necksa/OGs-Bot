67ea0606c67e368f6312764b5b1dd216cc49e33f
Necksa <107913288+Necksa@users.noreply.github.com>
2026-06-08 05:51:18 +0530

    Update mapban.js

--- systems/mapban.js
+++ systems/mapban.js
@@ -10,7 +10,18 @@ const MAP_POOL = [
   'split'
 ];
 
+const mapImages = {
+  ascent: './photos/maps/ascent.webp',
+  breeze: './photos/maps/breeze.webp',
+  fracture: './photos/maps/fracture.webp',
+  haven: './photos/maps/haven.webp',
+  lotus: './photos/maps/lotus.webp',
+  pearl: './photos/maps/pearl.webp',
+  split: './photos/maps/split.webp'
+};
+
 module.exports = {
   activeMapBans,
-  MAP_POOL
+  MAP_POOL,
+  mapImages
 };
